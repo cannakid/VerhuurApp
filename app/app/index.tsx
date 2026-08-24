c744089a65845517ea5910b8304500bbb73b21cf
Jonas Vaerewijck <jonasvaerewijck@gmail.com>
2026-08-24 16:25:52 +0200

    make login almost work

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,7 +1,20 @@
-import { Text, TextInput, View, StyleSheet } from "react-native";
+import { Text, TextInput, View, StyleSheet, Pressable } from "react-native";
 import Constants from "expo-constants";
+import { useState } from "react";
 
 export default function Index() {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  
+  
+  
+  const submit = () => {
+    console.log(username);
+    console.log(password);
+    setUsername("");
+    setPassword("")
+  }
+
   return (
     <View
       style={{
@@ -28,19 +41,30 @@ export default function Index() {
           placeholder="username"
           keyboardType="default"
           style={styles.textinput}
+          value={username}
+          onChangeText={(text) => setUsername(text)}
           ></TextInput>
+        
         <TextInput 
           secureTextEntry={true}
           placeholder="password"
           keyboardType="default"
           style={styles.textinput}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
           ></TextInput>
+          <Pressable
+        onPress={submit}
+        ><Text>Submit</Text></Pressable>
       </View>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
     container: { paddingTop: Constants.statusBarHeight},
     textinput: { borderColor: "red", borderWidth: 1, width: 200}
-});
+}); 
+
+
